37f744e5da07659ae1315a3947e122ead2dbc51d
Roman Hosek <romanhosek.cz@gmail.com>
2020-12-10 15:32:35 +0100

    example

--- examples/reactnative/App.js
+++ examples/reactnative/App.js
@@ -24,17 +24,24 @@ export default class example extends Component {
     currentVersion: null,
     latestVersion: null,
     isNeeded: false,
+    canSkip: true,
+    minimalVersion: null,
+    forcedVersion: null,
+    blackList: null,
     storeUrl: '',
   };
   componentDidMount() {
     VersionCheck.needUpdate({
-      latestVersion: '1.0.0',
+      currentVersion:'1.1.8',
+      provider: 'jsonFile',
+      url: 'https://raw.githubusercontent.com/hosek/react-native-version-check/master/version.json',
+      platform: 'android'
     }).then(res => this.setState(res));
 
-    VersionCheck.getStoreUrl({ appID: '364709193' }).then(res => {
-      //App Store ID for iBooks.
-      this.setState({ storeUrl: res });
-    });
+    // VersionCheck.getStoreUrl({ appID: '364709193' }).then(res => {
+    //   //App Store ID for iBooks.
+    //   this.setState({ storeUrl: res });
+    // });
   }
   render() {
     return (
@@ -46,7 +53,19 @@ export default class example extends Component {
           Latest Version: {this.state.latestVersion}
         </Text>
         <Text style={styles.text}>
+          Minimal Version: {this.state.minimalVersion}
+        </Text>
+        <Text style={styles.text}>
+          Forced Version: {this.state.forcedVersion}
+        </Text>
+        <Text style={styles.text}>
+          Blacklist: {this.state.blackList}
+        </Text>
+        <Text style={styles.text}>
           Is update needed?: {String(this.state.isNeeded)}
+        </Text>
+        <Text style={styles.text}>
+          Can skip?: {String(this.state.canSkip)}
         </Text>
         <View>
           <Text style={styles.text}>Store Url:</Text>
